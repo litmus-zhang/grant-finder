28f7265cbf09fa31f9ad15a656bb00f72a39ad51
litmus-zhang <abdulsalamlukmon9@gmail.com>
2023-04-22 16:35:42 +0100

    updated the source code

--- src/main.ts
+++ src/main.ts
@@ -7,9 +7,21 @@ const crawler = new PlaywrightCrawler({
     // Use the requestHandler to process each of the crawled pages.
     async requestHandler({ request, page, enqueueLinks, log }) {
         console.log(`Processing ${request.url}...`);
+        // click on the ecosystem label
 
 
-        if (request.label === 'Card Detail') {
+        if (request.loadedUrl === 'https://blockworks.co/grants/programs') {
+            await page.waitForSelector('label > .flex.flex-col');
+            await page.click('text=Select ecosystem to filter')
+            await page.click('text=Solana')
+
+            await enqueueLinks({
+                selector: 'tbody > tr > td > a.text-gray-900',
+                label: 'Card Detail',
+                strategy: 'all'
+            })
+
+        } else if (request.label === 'Card Detail') {
             const link = request.url;
             const title = await page.title();
 
@@ -17,12 +29,11 @@ const crawler = new PlaywrightCrawler({
                 link,
                 title,
             }
-            // console.log(result);
+            log.info(`result: ${result}`);
             await Dataset.pushData(result);
 
             await Dataset.exportToJSON('OUTPUT', { toKVS: 'my-data' })
-        }
-        else {
+        } else {
             await page.waitForSelector('.notion-collection-card')
             await page.click
             // Add all links found on the page to the queue.
@@ -32,9 +43,36 @@ const crawler = new PlaywrightCrawler({
             });
         }
 
-    }
+        //  Superteam scraping logic
+        // if (request.label === 'Card Detail') {
+        //     const link = request.url;
+        //     const title = await page.title();
+
+        //     const result = {
+        //         link,
+        //         title,
+        //     }
+        //     // console.log(result);
+        //     await Dataset.pushData(result);
+
+        //     await Dataset.exportToJSON('OUTPUT', { toKVS: 'my-data' })
+        // }
+        // else {
+        //     await page.waitForSelector('.notion-collection-card')
+        //     await page.waitForSelector('.notion-collection-card__title')
+        //     // Add all links found on the page to the queue.
+        //     await enqueueLinks({
+        //         selector: '.notion-colle',
+        //         label: 'Card Detail'
+        //     });
+        // }
+
+    },
+    // headless: false,
+
 
 });
 
 // Add first URL to the queue and start the crawl.
-await crawler.run(['https://superteam.fun/instagrants']);
+await crawler.run(['https://superteam.fun/instagrants', 'https://blockworks.co/grants/programs']);
+//await crawler.run(['https://superteam.fun/instagrants']);
